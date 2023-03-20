0538c9a612c259224af974500197cf403755c4d8
Lucas Infante <maccinza@gmail.com>
2023-03-20 17:05:47 -0300

    Uncomments important piece of code

--- sortinator/producer.py
+++ sortinator/producer.py
@@ -14,23 +14,23 @@ def produce():
     start = get_utc_timestamp()
 
     first_queue_tasks = []
-    # second_queue_tasks = []
+    second_queue_tasks = []
     for _ in range(NUM_TASKS):
         first_queue_tasks.append(
             sortinate.s().set(queue="first_testing")
         )
-    # for _ in range(NUM_TASKS//2):
-    #     second_queue_tasks.append(
-    #         sortinate.s().set(queue="second_testing")
-    #     )
+    for _ in range(NUM_TASKS//2):
+        second_queue_tasks.append(
+            sortinate.s().set(queue="second_testing")
+        )
 
     res_one = chord(first_queue_tasks)(
         summarize.s().set(queue="first_testing")
     )
 
-    # res_two = chord(second_queue_tasks)(
-    #     summarize.s().set(queue="second_testing")
-    # )
+    res_two = chord(second_queue_tasks)(
+        summarize.s().set(queue="second_testing")
+    )
 
     elapsed_time = get_utc_timestamp() - start
     logger.info(
@@ -38,7 +38,7 @@ def produce():
         f"seconds."
     )
     logger.info(f"Chord One: {res_one}")
-    # logger.info(f"Chord Two: {res_two}")
+    logger.info(f"Chord Two: {res_two}")
 
 
 if __name__ == "__main__":
